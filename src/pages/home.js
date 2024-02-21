e476d948d0b87495d9268198a85fd779df586548
Gabriela Lieira <gabslieira@gmail.com>
2024-02-21 19:30:01 -0300

    finalização da seção sobre o jogo

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,8 +3,10 @@ import { useNavigate } from "react-router";
 import "./style.scss";
 import logoGame from "./assets/logo.png"
 import logoGhibli from "./assets/logoGhibli.png"
-import decorationImg from "./assets/decoration-img.jpg"
-import decorationImg2 from "./assets/decoration-img2.jpg"
+import aboutImgOne from "./assets/about-img-one.jpg"
+import aboutImgTwo from "./assets/about-img-two.jpg"
+import aboutImgThree from "./assets/about-img-three.jpg"
+import aboutImgFour from "./assets/about-img-four.jpg"
 
 const Home = () => {
 
@@ -39,14 +41,21 @@ const Home = () => {
         <div className="test"></div>
 
         <section className="about-game">
-            <div className="about-text">
-                <h1>SOBRE O JOGO</h1>
-                <p></p>
-            </div>
-
-            <div className="decoration-image">
-                <img src={decorationImg} />
-                <img src={decorationImg2} />
+            <div className="details-circle-right"></div>
+            <div className="datails-circle-left"></div>
+            <div className="about">
+                <div className="about-text">
+                    <h1>SOBRE O JOGO</h1>
+                    <p className="description"> Em "Howl's Moving Castle", Sophie, uma jovem transformada em uma velha por uma bruxa, embarca em 
+                        uma jornada com o feiticeiro Howl para desfazer o feitiço, enfrentando desafios e descobrindo a 
+                        magia da autoaceitação ao longo do caminho. </p>
+                    <p className="atention-text">
+                        ** Howl's Moving Castle trata-se de um anime produzido pelo Studio Ghibli, este jogo RPG não existe,
+                        é apenas um projeto ilustrativo. 
+                    </p>
+                </div>
+                <img className="about-img" src={aboutImgOne} />
+                
             </div>
         </section>
       </>
